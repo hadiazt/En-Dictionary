73c49f87dd2a19fc1b9f4fc7050bcf9b466ca7e3
hadi <65118892+hadiazt@users.noreply.github.com>
2022-02-17 22:38:14 +0330

    An Backup For Tests

--- routes/index.js
+++ routes/index.js
@@ -6,19 +6,46 @@ const dictionary = new CambDict.Dictionary();
 
 var OUTPUT = []
 var ERR = []
+
 router.get("/", (req, res) => {
     var WORD = req.query.word
+
     if (WORD) {
-        dictionary.meaning(WORD).then(response => { OUTPUT.push(response) }).catch(e => { ERR.push(e) })
+        dictionary.meaning(WORD).then(response => {
+            OUTPUT.push({
+                'word': response.word,
+                'meaning': response.meaning,
+                'pronounciation': response.pronounciation,
+                'type': response.type,
+                'examples': response.examples,
+                'audio': response.audio
+            })
+        }).catch(e => { ERR.push(e) && console.log(e) })
+
+        if (OUTPUT || ERR) {
+            setTimeout(() => {
+                res.render("index", {
+                    icon: "https://cdn-icons-png.flaticon.com/512/1902/1902654.png",
+                    pageTitle: "EN Dictionary",
+                    WORD,
+                    OUTPUT: OUTPUT[0],
+                    ERR,
+                    layout: "./"
+                });
+                console.log(ERR);
+            }, 6000);
+        }
+    } else if (WORD) {
+        res.render("index", {
+            icon: "https://cdn-icons-png.flaticon.com/512/1902/1902654.png",
+            pageTitle: "EN Dictionary",
+            WORD,
+            OUTPUT: OUTPUT,
+            ERR: ERR,
+            layout: "./"
+        });
     }
-    res.render("index", {
-        icon: "https://cdn-icons-png.flaticon.com/512/1902/1902654.png",
-        pageTitle: "EN Dictionary",
-        WORD,
-        OUTPUT: OUTPUT[0],
-        ERR: ERR[0],
-        layout: "./"
-    });
+
 
 
 });
