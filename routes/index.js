7a7de1a78cb1093e319b54ed17b9ed6710e4883f
hadi <65118892+hadiazt@users.noreply.github.com>
2022-02-17 21:57:57 +0330

    Better Writing No More Error

--- routes/index.js
+++ routes/index.js
@@ -3,16 +3,20 @@ const router = new Router();
 
 const CambDict = require("camb-dict");
 const dictionary = new CambDict.Dictionary();
-dictionary.meaning("dictionary").then(console.log).catch(console.error);
 
+var OUTPUT = []
+var ERR = []
 router.get("/", (req, res) => {
     var WORD = req.query.word
-
+    if (WORD) {
+        dictionary.meaning(WORD).then(response => { OUTPUT.push(response) }).catch(e => { ERR.push(e) })
+    }
     res.render("index", {
         icon: "https://cdn-icons-png.flaticon.com/512/1902/1902654.png",
         pageTitle: "EN Dictionary",
         WORD,
-        dictionary,
+        OUTPUT: OUTPUT[0],
+        ERR: ERR[0],
         layout: "./"
     });
 
